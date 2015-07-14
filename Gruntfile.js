afc711ec73268fbd7f76d7e680e395a15a2296ba
CBarrett2 <chris.barrett2@hotmail.co.uk>
2015-07-14 15:18:04 +0100

    Added Gruntfile and examples in src

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,75 @@
-module.exports = function(grunt) {
+/* Global module */
 
-  grunt.initConfig({
-  });
+module.exports = function (grunt) {
+    'use strict';
+    grunt.initConfig({ 
+        pkg: grunt.file.readJSON('package.json'),
+        
+        meta: {
+            componentsJsFiles: [
+                'src/**/*.js'
+            ],
+            testJsFiles: [
+                'test/**/*Spec.js'
+            ],
+            ourJsFiles: [
+                '<%= meta.componentsJsFiles %>',
+                '<%= meta.testJsFiles %>',
+            ]
+        },
+    
+        jscs: {
+            options: {
+                config: '.jscsrc'
+            },
+            failOnError: {
+                files: {
+                    src: ['<%= meta.ourJsFiles %>']
+                }
+            },
+            warnOnly: {
+                options: {
+                    force: true
+                },
+                files: {
+                    src: ['<%= meta.ourJsFiles %>']
+                }
+            }
+        },
 
-  grunt.registerTask('default', []);
-
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            failOnError: {
+                files: {
+                    src: ['<%= meta.ourJsFiles %>']
+                }
+            },
+            warnOnly: {
+                options: {
+                    force: true
+                },
+                files: {
+                    src: ['<%= meta.ourJsFiles %>']
+                }
+            }
+        },
+        
+        watch: {
+            files: ['<%= meta.ourJsFiles %>'],
+            tasks: ['jshint', 'jscs']
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.registerTask('default', ['jshint', 'jscs']);
+    grunt.registerTask('check:failOnError', ['jshint:failOnError', 'jscs:failOnError']);
+    grunt.registerTask('check:warnOnly', ['jshint:warnOnly', 'jscs:warnOnly']);
+    grunt.registerTask('check', ['check:failOnError']);
+    grunt.registerTask('check:failOnError', ['jshint:failOnError', 'jscs:failOnError']);
+    grunt.registerTask('check:warnOnly', ['jshint:warnOnly', 'jscs:warnOnly']);
 };
---
+++ src/main.js
@@ -0,0 +1,39 @@
+/* A test function */
+
+// Main function
+(function() {
+    'use strict';
+
+    var width = 500;
+    var height = 300;
+
+    var svg = d3.select('#chart-example')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    var data = fc.dataGenerator()(150);
+
+    var chart = fc.chart.linearTimeSeries()
+        .xDomain(fc.util.extent(data, 'date'))
+        .yDomain(fc.util.extent(data, ['open', 'close']));
+
+    svg.datum(data)
+        .call(chart);
+
+    var area = fc.series.area()
+        .yValue(function(d) { return d.open; });
+
+    var line = fc.series.line()
+        .yValue(function(d) { return d.open; });
+
+    var gridlines = fc.annotation.gridline()
+        .yTicks(5)
+        .xTicks(0);
+
+    var multi = fc.series.multi()
+        .series([gridlines, area, line]);
+
+    chart.plotArea(multi);
+})();
+
