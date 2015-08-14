ba3f5b9f0b45929b5551250299219e6fefd4aeea
Drew Miley <dmiley@scottlogic.co.uk>
2015-08-14 12:45:29 +0100

    Refactored resizing method into own file

---
+++ src/assets/js/util/calculateDimensions.js
@@ -0,0 +1,29 @@
+(function(d3, fc) {
+    'use strict';
+
+    sc.util.calculateDimensions = function(container) {
+        var leftPadding = parseInt(container.select('.col-md-12').style('padding-left'), 10);
+        var rightPadding = parseInt(container.select('.col-md-12').style('padding-right'), 10);
+
+        var headRowHeight = parseInt(container.select('#head-row').style('height'), 10);
+        var navHeight = parseInt(container.select('svg.nav').style('height'), 10);
+
+        var useableScreenWidth = parseInt(container.style('width'), 10) - (leftPadding + rightPadding);
+        var useableScreenHeight = window.innerHeight - headRowHeight - navHeight -
+            2 * fc.chart.linearTimeSeries().xAxisHeight();
+
+        var targetWidth = useableScreenWidth;
+        var targetHeight = useableScreenHeight;
+
+        var mainHeightRatio = 0.6;
+        var rsiHeightRatio = 0.3;
+        var totalHeightRatio = mainHeightRatio + rsiHeightRatio;
+
+        container.select('svg.main').attr('width', targetWidth)
+            .attr('height', mainHeightRatio * targetHeight / totalHeightRatio);
+        container.select('svg.rsi').attr('width', targetWidth)
+            .attr('height', rsiHeightRatio * targetHeight / totalHeightRatio);
+        container.select('svg.nav').attr('width', targetWidth);
+    };
+
+})(d3, fc);
