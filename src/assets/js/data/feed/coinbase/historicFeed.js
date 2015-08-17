9a9240ef043538498f8b1fbe781d46fcb19fa13b
CBarrett2 <chris.barrett2@hotmail.co.uk>
2015-08-17 13:56:06 +0100

    Now loads historic data and initialized live feed

---
+++ src/assets/js/data/feed/coinbase/historicFeed.js
@@ -0,0 +1,30 @@
+(function(sc) {
+    'use strict';
+
+    sc.data.feed.coinbase.historicFeed = function() {
+        // In seconds - maybe take this out and have chart pass in a period whenever needed
+        var feed = fc.data.feed.coinbase();
+        var n = 0;
+
+        function historicFeed(callback) {
+            var id = ++n;
+            feed(function(err, newData) {
+                if (id < n) { return; }
+                if (!err) {
+                    newData = newData.reverse();
+                    callback(null, newData);
+                } else { callback(err, null); }
+            });
+        }
+
+        d3.rebind(historicFeed, feed, 'granularity', 'start', 'end');
+
+        historicFeed.invalidateCallback = function() {
+            n++;
+            return historicFeed;
+        };
+
+        return historicFeed;
+    };
+
+})(sc);
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -95,7 +95,7 @@
     var currDate = new Date();
     var startDate = d3.time.minute.offset(currDate, -199);
 
-    var coinbase = fc.data.feed.coinbase()
+    var historicFeed = sc.data.feed.coinbase.historicFeed()
         .granularity(60)
         .start(startDate)
         .end(currDate);
@@ -126,7 +126,7 @@
 
     function historicCallback(err, newData) {
         if (!err) {
-            data = newData.reverse();
+            data = newData;
             resetToLive();
             ohlcConverter(liveCallback);
             render();
@@ -145,12 +145,13 @@
             if (type === 'live') {
                 data = [];
                 toggleLiveFeedUI(true);
-                coinbase(historicCallback);
+                historicFeed(historicCallback);
                 render();
 
             } else if (type === 'fake') {
                 toggleLiveFeedUI(false);
                 ohlcConverter.close();
+                historicFeed.invalidateCallback();
                 data = fc.data.random.financial()(250);
                 resetToLive();
                 render();
@@ -225,32 +226,6 @@
             .call(navChart);
     }
 
-    var multi = fc.series.multi()
-        .series([gridlines, ma, currentSeries, closeAxisAnnotation])
-        .mapping(function(series) {
-            switch (series) {
-                case closeAxisAnnotation:
-                    return [data[data.length - 1]];
-                default:
-                    return data;
-            }
-        })
-        .key(function(series, index) {
-            switch (series) {
-                case line:
-                    return index;
-                default:
-                    return series;
-            }
-        });
-
-    function zoomCall(zoom, selection, data, scale) {
-        return function() {
-            sc.util.zoomControl(zoom, selection, data, scale);
-            render();
-        };
-    }
-    
     function resize() {
         sc.util.calculateDimensions(container);
         render();
