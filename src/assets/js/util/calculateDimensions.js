4011504bfb30fe04e5e692d45bc54b8253eb8436
Drew Miley <dmiley@scottlogic.co.uk>
2015-08-19 11:19:53 +0100

    Removed svg dependence in dimension calculating

--- src/assets/js/util/calculateDimensions.js
+++ src/assets/js/util/calculateDimensions.js
@@ -2,15 +2,11 @@
     'use strict';
 
     sc.util.calculateDimensions = function(container, secondaryChartShown) {
-        var leftPadding = parseInt(container.select('.col-md-12').style('padding-left'), 10);
-        var rightPadding = parseInt(container.select('.col-md-12').style('padding-right'), 10);
-
         var headRowHeight = parseInt(container.select('#head-row').style('height'), 10) +
             parseInt(container.select('#head-row').style('padding-top'), 10) +
             parseInt(container.select('#head-row').style('padding-bottom'), 10);
-        var navHeight = parseInt(container.select('svg.nav').style('height'), 10);
+        var navHeight = parseInt(container.select('#nav-row').style('height'), 10);
 
-        var useableScreenWidth = parseInt(container.style('width'), 10) - (leftPadding + rightPadding);
         var useableScreenHeight = window.innerHeight - headRowHeight - navHeight;
 
         var primaryHeightRatio = secondaryChartShown ? 2 : 1;
@@ -18,11 +14,10 @@
 
         var totalHeightRatio = primaryHeightRatio + secondaryHeightRatio;
 
-        container.select('svg.primary').attr('width', useableScreenWidth)
-            .attr('height', primaryHeightRatio * useableScreenHeight / totalHeightRatio);
-        container.select('svg.secondary').attr('width', useableScreenWidth)
-            .attr('height', secondaryHeightRatio * useableScreenHeight / totalHeightRatio);
-        container.select('svg.nav').attr('width', useableScreenWidth);
+        container.select('#primary-row')
+            .style('height', primaryHeightRatio * useableScreenHeight / totalHeightRatio + 'px');
+        container.select('#secondary-row')
+            .style('height', secondaryHeightRatio * useableScreenHeight / totalHeightRatio + 'px');
     };
 
 })(d3, fc);
