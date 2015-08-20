38f7a1bd06d1b8cdb37783e4ada9f130988ee743
CBarrett2 <chris.barrett2@hotmail.co.uk>
2015-08-20 16:35:17 +0100

    split out series definitions

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -13,8 +13,6 @@
         viewDomain: []
     };
 
-    sc.util.calculateDimensions(container);
-
     var primaryChart = sc.chart.primaryChart();
     var secondaryChart = null;
     var navChart = sc.chart.navChart();
@@ -23,6 +21,15 @@
         .on('optionChange', function(seriesType) {
             primaryChart.changeSeries(seriesType.series);
             render();
+        });
+
+    var secondaryChartOptions = sc.menu.optionGenerator()
+        .on('optionChange', function(secondaryChartType) {
+            secondaryChart = secondaryChartType.chart;
+            if (secondaryChart) {
+                secondaryChart.on('viewChange', onViewChanged);
+            }
+            resize();
         });
 
     var indicatorOptions = sc.menu.optionGenerator()
@@ -39,64 +46,17 @@
     primaryChart.on('viewChange', onViewChanged);
     navChart.on('viewChange', onViewChanged);
 
-    var SeriesType = function(displayString, valueString, series) {
-        this.displayString = displayString;
-        this.valueString = valueString;
-        this.series = series;
-    };
+    var seriesButtons = container.select('#series-buttons');
+    sc.util.bindSeriesToContainer(seriesButtons);
+    seriesButtons.call(seriesOptions);
 
-    var candlestick = new SeriesType('Candlestick', 'candlestick', sc.series.candlestick());
-    var ohlc = new SeriesType('OHLC', 'ohlc', fc.series.ohlc());
-    var line = new SeriesType('Line', 'line', fc.series.line());
-    line.series.isLine = true;
-    var point = new SeriesType('Point', 'point', fc.series.point());
-    var area = new SeriesType('Area', 'area', fc.series.area());
+    var secondaryChartButtons = container.select('#secondary-chart-buttons');
+    sc.util.bindSecondarySeriesToContainer(secondaryChartButtons);
+    secondaryChartButtons.call(secondaryChartOptions);
 
-    container.select('#series-buttons')
-        .datum([candlestick, ohlc, line, point, area])
-        .call(seriesOptions);
-
-    var IndicatorType = function(displayString, valueString, indicator) {
-        this.displayString = displayString;
-        this.valueString = valueString;
-        this.indicator = indicator;
-    };
-
-    var movingAverage = fc.series.line()
-        .decorate(function(select) {
-            select.enter().classed('movingAverage', true);
-        })
-        .yValue(function(d) { return d.movingAverage; });
-
-    var noIndicator = new IndicatorType('None', 'no-indicator', null);
-    var movingAverageIndicator = new IndicatorType('Moving Average', 'movingAverage', movingAverage);
-    var bollingerIndicator = new IndicatorType('Bollinger Bands', 'bollinger', fc.indicator.renderer.bollingerBands());
-
-    container.select('#indicator-buttons')
-        .datum([noIndicator, movingAverageIndicator, bollingerIndicator])
-        .call(indicatorOptions);
-
-    var secondaryChartOptions = sc.menu.optionGenerator()
-        .on('optionChange', function(secondaryChartType) {
-            secondaryChart = secondaryChartType.chart;
-            if (secondaryChart) {
-                secondaryChart.on('viewChange', onViewChanged);
-            }
-            resize();
-        });
-
-    var SecondaryChartType = function(displayString, valueString, chart) {
-        this.displayString = displayString;
-        this.valueString = valueString;
-        this.chart = chart;
-    };
-
-    var noChart = new SecondaryChartType('None', 'no-chart', null);
-    var rsiChart = new SecondaryChartType('RSI', 'rsi', sc.chart.rsiChart());
-
-    container.select('#secondary-chart-buttons')
-        .datum([noChart, rsiChart])
-        .call(secondaryChartOptions);
+    var indicatorButtons = container.select('#indicator-buttons');
+    sc.util.bindIndicatorsToContainer(indicatorButtons);
+    indicatorButtons.call(indicatorOptions);
 
     // Set Reset button event
     function resetToLive() {
@@ -198,6 +158,6 @@
 
     d3.select(window).on('resize', resize);
 
+    sc.util.calculateDimensions(container);
     resetToLive();
-    resize();
 })(d3, fc, sc);
