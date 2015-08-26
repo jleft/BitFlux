cd5ddfadbea46c10d6179420ee2e993edb1b9fa2
CBarrett2 <chris.barrett2@hotmail.co.uk>
2015-08-26 15:36:44 +0100

    Split definitions and binding into separate function

---
+++ src/assets/js/util/primarySeriesBinding.js
@@ -0,0 +1,20 @@
+(function(d3, fc, sc) {
+    'use strict';
+    // Define primary series types
+    var SeriesType = function(displayString, valueString, series) {
+        this.displayString = displayString;
+        this.valueString = valueString;
+        this.series = series;
+    };
+
+    var candlestick = new SeriesType('Candlestick', 'candlestick', fc.series.candlestick());
+    var ohlc = new SeriesType('OHLC', 'ohlc', fc.series.ohlc());
+    var line = new SeriesType('Line', 'line', fc.series.line());
+    line.series.isLine = true;
+    var point = new SeriesType('Point', 'point', fc.series.point());
+    var area = new SeriesType('Area', 'area', fc.series.area());
+
+    sc.util.bindSeriesToContainer = function(selection) {
+        selection.datum([candlestick, ohlc, line, point, area]);
+    };
+})(d3, fc, sc);
---
+++ src/assets/js/util/secondarySeriesBinding.js
@@ -0,0 +1,16 @@
+(function(d3, fc, sc) {
+    'use strict';
+    // Define secondary series types
+    var SecondaryChartType = function(displayString, valueString, chart) {
+        this.displayString = displayString;
+        this.valueString = valueString;
+        this.chart = chart;
+    };
+
+    var noChart = new SecondaryChartType('None', 'no-chart', null);
+    var rsiChart = new SecondaryChartType('RSI', 'rsi', sc.chart.rsiChart());
+
+    sc.util.bindSecondarySeriesToContainer = function(selection) {
+        selection.datum([noChart, rsiChart]);
+    };
+})(d3, fc, sc);
