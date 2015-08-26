d5b2fb435a39b23b335c9a453f025a5b5fecd53f
James Leftley <jleftley@scottlogic.com>
2015-08-26 10:07:39 +0100

    Concat JavaScript source files into a single file and add tests for filterDataInDateRange

---
+++ test/util/filterDataInDateRangeSpec.js
@@ -0,0 +1,85 @@
+(function(d3, fc, sc) {
+    'use strict';
+
+    describe('sc.util.filterDataInDateRange', function() {
+
+        function obj(val) {
+            return {
+                date: val
+            };
+        }
+
+        var data;
+        var monday = new Date(2015, 7, 17);
+        var tuesday = new Date(2015, 7, 18);
+        var wednesday = new Date(2015, 7, 19);
+        var thursday = new Date(2015, 7, 20);
+        var friday = new Date(2015, 7, 21);
+
+        beforeEach(function() {
+            data = [obj(monday), obj(tuesday), obj(wednesday), obj(thursday), obj(friday)];
+        });
+
+
+        it('should return all data (without padding) when date range covers the data\'s date extent', function() {
+            var filteredData = sc.util.filterDataInDateRange(data, [data[0].date, data[data.length - 1].date]);
+
+            expect(filteredData.length).toEqual(data.length);
+            expect(filteredData[0].date).toEqual(monday);
+            expect(filteredData[1].date).toEqual(tuesday);
+            expect(filteredData[2].date).toEqual(wednesday);
+            expect(filteredData[3].date).toEqual(thursday);
+            expect(filteredData[4].date).toEqual(friday);
+        });
+
+        it('should pad either side of the data when the date range is within the data\'s date extent', function() {
+            var filteredData = sc.util.filterDataInDateRange(data, [wednesday, wednesday]);
+
+            expect(filteredData.length).toEqual(3);
+            expect(filteredData[0].date).toEqual(tuesday);
+            expect(filteredData[1].date).toEqual(wednesday);
+            expect(filteredData[2].date).toEqual(thursday);
+        });
+
+        it('should pad the start of the data' +
+            'when the date range\'s end date is not at the end of the data\'s date extent', function() {
+            var filteredData = sc.util.filterDataInDateRange(data, [monday, tuesday]);
+
+            expect(filteredData.length).toEqual(3);
+            expect(filteredData[0].date).toEqual(monday);
+            expect(filteredData[1].date).toEqual(tuesday);
+            expect(filteredData[2].date).toEqual(wednesday);
+        });
+
+        it('should pad the end of the data' +
+            'when the date range\'s start date is not at the start of the data\'s date extent',
+            function() {
+            var filteredData = sc.util.filterDataInDateRange(data, [thursday, friday]);
+
+            expect(filteredData.length).toEqual(3);
+            expect(filteredData[0].date).toEqual(wednesday);
+            expect(filteredData[1].date).toEqual(thursday);
+            expect(filteredData[2].date).toEqual(friday);
+        });
+
+        it('should return the first date when a date range is specified before the first datum\'s date', function() {
+            var filteredData = sc.util.filterDataInDateRange(data, [
+                d3.time.day.offset(monday, -2),
+                d3.time.day.offset(monday, -1)]);
+
+            expect(filteredData.length).toEqual(1);
+            expect(filteredData[0].date).toEqual(monday);
+        });
+
+        it('should return the last date when a date range is specified after the last datum\'s date', function() {
+            var filteredData = sc.util.filterDataInDateRange(data, [
+                d3.time.day.offset(friday, 1),
+                d3.time.day.offset(friday, 2)]);
+
+            expect(filteredData.length).toEqual(1);
+            expect(filteredData[0].date).toEqual(friday);
+        });
+
+    });
+
+}(d3, fc, sc));
