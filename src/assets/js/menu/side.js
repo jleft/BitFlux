8d95639b5c2a3560af4bc11e0e2fa4cd1bedbe09
Tom Simmons <tlsim@users.noreply.github.com>
2015-10-21 22:09:18 +0100

    Menu refactor - remove sc.menu.group

--- src/assets/js/menu/side.js
+++ src/assets/js/menu/side.js
@@ -9,6 +9,7 @@
             sc.event.primaryChartIndicatorChange,
             sc.event.secondaryChartChange);
 
+        // TODO: Move up to model.
         var candlestick = sc.menu.option('Candlestick', 'candlestick', sc.series.candlestick());
         var ohlc = sc.menu.option('OHLC', 'ohlc', fc.series.ohlc());
         var line = sc.menu.option('Line', 'line', fc.series.line());
@@ -16,25 +17,24 @@
         var point = sc.menu.option('Point', 'point', fc.series.point());
         var area = sc.menu.option('Area', 'area', fc.series.area());
 
-        var primaryChartSeriesOptions = sc.menu.group()
-            .formOptionListFromCollection([candlestick, ohlc, line, point, area], fc.util.fn.identity)
-            .generator(sc.menu.generator.buttonGroup())
+
+        var primaryChartSeriesOptions = sc.menu.generator.buttonGroup()
             .on('optionChange', function(series) {
                 dispatch[sc.event.primaryChartSeriesChange](series);
             });
 
+        // TODO: Move up to model (or remove, see #263)
         var open = sc.menu.option('Open', 'open', function(d) { return d.open; });
         var high = sc.menu.option('High', 'high', function(d) { return d.high; });
         var low = sc.menu.option('Low', 'low', function(d) { return d.low; });
         var close = sc.menu.option('Close', 'close', function(d) { return d.close; });
 
-        var primaryChartYValueAccessorOptions = sc.menu.group()
-            .formOptionListFromCollection([open, high, low, close], fc.util.fn.identity)
-            .generator(sc.menu.generator.buttonGroup(3))
+        var primaryChartYValueAccessorOptions = sc.menu.generator.buttonGroup(3) // TODO: '3' on model property.
             .on('optionChange', function(yValueAccessor) {
                 dispatch[sc.event.primaryChartYValueAccessorChange](yValueAccessor);
             });
 
+        // TODO: Move up to model.
         var movingAverage = fc.series.line()
             .decorate(function(select) {
                 select.enter()
@@ -45,34 +45,41 @@
         var movingAverageIndicator = sc.menu.option('Moving Average', 'movingAverage', movingAverage);
         var bollingerIndicator = sc.menu.option('Bollinger Bands', 'bollinger', fc.indicator.renderer.bollingerBands());
 
-        var primaryChartIndicatorToggle = sc.menu.group()
-            .formOptionListFromCollection([movingAverageIndicator, bollingerIndicator], fc.util.fn.identity)
-            .generator(sc.menu.generator.toggleGroup())
+        var primaryChartIndicatorToggle = sc.menu.generator.toggleGroup()
             .on('optionChange', function(indicator) {
                 dispatch[sc.event.primaryChartIndicatorChange](indicator);
             });
 
+        // TODO: Move up to model.
         var rsi = sc.menu.option('RSI', 'secondary-rsi', sc.chart.rsi());
         var macd = sc.menu.option('MACD', 'secondary-macd', sc.chart.macd());
         var volume = sc.menu.option('Volume', 'secondary-volume', sc.chart.volume());
 
-        var secondaryChartToggle = sc.menu.group()
-            .formOptionListFromCollection([rsi, macd, volume], fc.util.fn.identity)
-            .generator(sc.menu.generator.toggleGroup())
+        var secondaryChartToggle = sc.menu.generator.toggleGroup()
             .on('optionChange', function(chart) {
                 dispatch[sc.event.secondaryChartChange](chart);
             });
 
         var side = function(selection) {
-            selection.each(function() {
+            // TODO: var model = selection.datum();
+            selection.each(function() { // TODO: Do we really need to support multiple selection here ?
                 var selection = d3.select(this);
+
                 selection.select('#series-buttons')
+                    // TODO: Rename optionList to options.
+                    .datum({optionList: [candlestick, ohlc, line, point, area]})
                     .call(primaryChartSeriesOptions);
+
                 selection.select('#y-value-accessor-buttons')
+                    .datum({optionList: [open, high, low, close]})
                     .call(primaryChartYValueAccessorOptions);
+
                 selection.select('#indicator-buttons')
+                    .datum({optionList: [movingAverageIndicator, bollingerIndicator]})
                     .call(primaryChartIndicatorToggle);
+
                 selection.select('#secondary-chart-buttons')
+                    .datum({optionList: [rsi, macd, volume]})
                     .call(secondaryChartToggle);
             });
         };
