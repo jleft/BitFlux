6b48c099003cc5d81b5c23fc254d1a5b34cb4618
jdhodges431 <jdhodges431@gmail.com>
2015-10-29 10:59:09 +0000

    Legend displayed inline

--- src/assets/js/chart/legend.js
+++ src/assets/js/chart/legend.js
@@ -7,15 +7,6 @@
         var formatTime;
         var lastDataPointDisplayed;
 
-        var legendComponent = fc.chart.legend().items([
-                ['date', function(d) { return formatTime(d.date); }],
-                ['open', function(d) { return formatPrice(d.open); }],
-                ['high', function(d) { return formatPrice(d.high); }],
-                ['low', function(d) { return formatPrice(d.low); }],
-                ['close', function(d) { return formatPrice(d.close); }],
-                ['volume', function(d) { return formatVolume(d.volume); }]
-            ]);
-
         function legend(selection) {
             selection.each(function(model) {
                 var container = d3.select(this);
@@ -26,9 +17,47 @@
 
                 if (model.data !== lastDataPointDisplayed) {
                     lastDataPointDisplayed = model.data;
-                    container.datum(model.data)
-                        .call(legendComponent);
+                    container.datum(model.data);
                 }
+
+                var d = container.datum();
+
+                var legendComponents =  [
+                    'T',
+                    formatTime(d.date),
+                    'O',
+                    formatPrice(d.open),
+                    'H',
+                    formatPrice(d.high),
+                    'L',
+                    formatPrice(d.low),
+                    'C',
+                    formatPrice(d.close),
+                    'V',
+                    formatVolume(d.volume)
+                ];
+
+                var span = container.select('p')
+                    .selectAll('span')
+                    .data(legendComponents);
+
+                span.text(function(d) {
+                    return d;
+                });
+
+                span.enter()
+                    .append('span')
+                    .text(function(d) {
+                        return d;
+                    });
+
+                span.each(function(d, i) {
+                    if ((i + 1) % 2 === 0) {
+                        this.setAttribute('class', 'legendValue');
+                    } else {
+                        this.setAttribute('class', 'legendLabel');
+                    }
+                });
             });
         }
 
