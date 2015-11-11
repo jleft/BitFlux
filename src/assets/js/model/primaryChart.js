955aae76e0a73c9f22e0c537bc7d8c02e4420a6c
Arnaud Fredon <afredon@scottlogic.com>
2015-11-11 17:31:55 +0000

    primary chart renderer now discovers metadata changes in the model

--- src/assets/js/model/primaryChart.js
+++ src/assets/js/model/primaryChart.js
@@ -2,15 +2,51 @@
     'use strict';
 
     sc.model.primaryChart = function(initialProduct) {
-        return {
+        var model = {
             data: [],
             trackingLatest: true,
             viewDomain: [],
-            series: sc.menu.option('Candlestick', 'candlestick', sc.series.candlestick()),
-            yValueAccessor: {option: function(d) { return d.close; }},
-            indicators: [],
-            product: initialProduct
+            metaChanged: false
         };
+
+        var _product = initialProduct;
+        Object.defineProperty(model, 'product', {
+            get: function() { return _product; },
+            set: function(newValue) {
+                _product = newValue;
+                model.metaChanged = true;
+            }
+        });
+
+        var _series = sc.menu.option('Candlestick', 'candlestick', sc.series.candlestick());
+        Object.defineProperty(model, 'series', {
+            get: function() { return _series; },
+            set: function(newValue) {
+                _series = newValue;
+                model.metaChanged = true;
+            }
+        });
+
+        var _yValueAccessor = {option: function(d) { return d.close; }};
+        Object.defineProperty(model, 'yValueAccessor', {
+            get: function() { return _yValueAccessor; },
+            set: function(newValue) {
+                _yValueAccessor = newValue;
+                model.metaChanged = true;
+            }
+        });
+
+        var _indicators = [];
+        Object.defineProperty(model, 'indicators', {
+            get: function() { return _indicators; },
+            set: function(newValue) {
+                _indicators = newValue;
+                model.metaChanged = true;
+            }
+        });
+
+
+        return model;
     };
 
 })(d3, fc, sc);
