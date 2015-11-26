361071dab0c9f125d9288bcf5d9f5dead9099074
Arnaud Fredon <afredon@scottlogic.com>
2015-11-26 16:34:21 +0000

    Move code for navReset from Nav to a new function, allowing to have only navbar-container stored in containers, thus reinstating the suspended layout.

---
+++ src/assets/js/menu/navigationReset.js
@@ -0,0 +1,28 @@
+(function(d3, fc, sc) {
+    'use strict';
+
+    sc.menu.navigationReset = function() {
+
+        var dispatch = d3.dispatch(sc.event.resetToLatest);
+
+        function navReset(selection) {
+            var model = selection.datum();
+
+            var resetButton = selection.selectAll('g')
+                .data([model]);
+
+            resetButton.enter()
+                .append('g')
+                .attr('class', 'reset-button')
+                .on('click', function() { dispatch[sc.event.resetToLatest](); })
+                .append('path')
+                .attr('d', 'M1.5 1.5h13.438L23 20.218 14.937 38H1.5l9.406-17.782L1.5 1.5z');
+
+            resetButton.classed('active', !model.trackingLatest);
+        }
+
+        d3.rebind(navReset, dispatch, 'on');
+
+        return navReset;
+    };
+})(d3, fc, sc);
---
+++ src/assets/js/model/navigationReset.js
@@ -0,0 +1,10 @@
+(function(d3, fc, sc) {
+    'use strict';
+
+    sc.model.navigationReset = function() {
+        return {
+            trackingLatest: true
+        };
+    };
+
+})(d3, fc, sc);
