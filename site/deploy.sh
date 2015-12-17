971324ecc5ae3209bde3c17c4e74307e524ee88b
James Leftley <jleftley@scottlogic.com>
2015-12-17 14:26:30 +0000

    Try Travis

--- site/deploy.sh
+++ site/deploy.sh
@@ -18,7 +18,6 @@ grunt ci
 
 cd ../..
 
-# Fix this...
 rm -rf master
 mkdir master
 rm -rf develop
@@ -26,3 +25,21 @@ mkdir develop
 cp -r temp/master/dist/* master
 cp -r temp/develop/dist/* develop
 rm -rf temp || exit 0;
+
+# create a *new* Git repo
+git init
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "<you>@<your-email>"
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
