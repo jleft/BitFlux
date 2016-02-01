ad22cd2c195877236919f1ce3ad78734a602d11d
jdhodges431 <jdhodges431@gmail.com>
2016-02-01 13:28:09 +0000

    Added option to seed random.
    Moved seedrandom to main.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,10 @@
+/*global window */
 import BitFlux from './bitflux';
+
+var seed = window.location.search.split('seed=')[1];
+if (seed) {
+    Math.seedrandom(seed);
+}
 
 BitFlux.app()
     .fetchCoinbaseProducts(true)
