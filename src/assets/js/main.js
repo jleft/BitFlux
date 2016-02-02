cc86bdf49424cfb8a35f3a8f7259897e6dadf6d8
James Leftley <jleftley@scottlogic.com>
2016-02-02 09:13:35 +0000

    Document usage of query string for seeding random number generator

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,9 @@
 /*global window */
 import BitFlux from './bitflux';
 
+// A query string (?seed=) can be added to the URL
+// to seed the random number generator.
+// For example: ?seed=yourseed
 var seed = window.location.search.split('seed=')[1];
 if (seed) {
     Math.seedrandom(seed);
