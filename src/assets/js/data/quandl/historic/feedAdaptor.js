9f6083509bb71ed91e165949d10ad8315359c612
James Leftley <jleftley@scottlogic.com>
2016-03-03 12:21:07 +0000

    Always use adjusted close for closing price with Quandl

--- src/assets/js/data/quandl/historic/feedAdaptor.js
+++ src/assets/js/data/quandl/historic/feedAdaptor.js
@@ -3,7 +3,9 @@ import fc from 'd3fc';
 
 export default function() {
 
-    var historicFeed = fc.data.feed.quandl(),
+    var historicFeed = fc.data.feed.quandl()
+            .database('WIKI')
+            .columnNameMap(mapColumnNames),
         granularity,
         candles;
 
@@ -12,10 +14,32 @@ export default function() {
     allowedPeriods.set(60 * 60 * 24, 'daily');
     allowedPeriods.set(60 * 60 * 24 * 7, 'weekly');
 
+    // Map fields for WIKI database, to use all adjusted values
+    var columnNameMap = d3.map();
+    columnNameMap.set('Open', 'unadjustedOpen');
+    columnNameMap.set('High', 'unadjustedHigh');
+    columnNameMap.set('Low', 'unadjustedLow');
+    columnNameMap.set('Close', 'unadjustedClose');
+    columnNameMap.set('Volume', 'unadjustedVolume');
+    columnNameMap.set('Adj. Open', 'open');
+    columnNameMap.set('Adj. High', 'high');
+    columnNameMap.set('Adj. Low', 'low');
+    columnNameMap.set('Adj. Close', 'close');
+    columnNameMap.set('Adj. Volume', 'volume');
+
+    function mapColumnNames(colName) {
+        var mappedName = columnNameMap.get(colName);
+        if (!mappedName) {
+            mappedName = colName[0].toLowerCase() + colName.substr(1);
+        }
+        return mappedName;
+    }
+
     function quandlAdaptor(cb) {
         var startDate = d3.time.second.offset(historicFeed.end(), -candles * granularity);
         historicFeed.start(startDate)
-            .collapse(allowedPeriods.get(granularity));
+            .collapse(allowedPeriods.get(granularity))
+            .dataset('AAPL');
         historicFeed(cb);
     }
 
