431af8dbe0f62ae7d03c273c282f64c6214d403f
Robert McNeill <rjmcneill@users.noreply.github.com>
2016-03-29 10:52:40 +0100

    Updated xAxis labelling to match new UX designs
    Updates after PR comments

--- src/assets/js/chart/xAxis.js
+++ src/assets/js/chart/xAxis.js
@@ -5,12 +5,13 @@ import util from '../util/util';
 export default function() {
     var xScale = fc.scale.dateTime();
 
-    var xAxis = d3.svg.axis()
+    var xAxis = fc.svg.axis()
       .scale(xScale)
       .orient('bottom');
 
     function preventTicksMoreFrequentThanPeriod(period) {
-        var scaleTickSeconds = (xScale.ticks()[1] - xScale.ticks()[0]) / 1000;
+        var scaleTickSeconds = (xScale.discontinuityProvider().distance(xScale.ticks()[0], xScale.ticks()[1])) / 1000;
+
         if (scaleTickSeconds < period.seconds) {
             xAxis.ticks(period.d3TimeInterval.unit, period.d3TimeInterval.value);
         } else {
@@ -18,13 +19,50 @@ export default function() {
         }
     }
 
+    function adaptTickFormatting() {
+        var scaleTickSeconds = (xScale.discontinuityProvider().distance(xScale.ticks()[0], xScale.ticks()[1])) / 1000;
+
+        var secondsInHour = 60 * 60;
+        var secondsInWeek = secondsInHour * 24 * 7;
+
+        var tickFormatting = d3.time.format.multi([
+            ['%H:%M,%d %b', function() { return (scaleTickSeconds / secondsInHour) <= 0.5; }],
+            ['%I %p,%a %d %b', function() { return (scaleTickSeconds / secondsInHour) <= 6; }],
+            ['%a %d,%b %Y', function() { return (scaleTickSeconds / secondsInWeek) <= 1; }],
+            ['%B,%Y', function() { return true; }]
+        ]);
+
+        xAxis.tickFormat(tickFormatting);
+    }
+
     function xAxisChart(selection) {
         var model = selection.datum();
+
         xScale.domain(model.viewDomain);
 
         xScale.discontinuityProvider(model.discontinuityProvider);
 
         preventTicksMoreFrequentThanPeriod(model.period);
+        adaptTickFormatting();
+
+        xAxis.decorate(function() {
+            var tickText = selection.selectAll('text');
+            tickText.each(function() {
+                var text = d3.select(this);
+                var split = text.text().split(',');
+                text.text(null);
+                text.append('tspan')
+                    .attr('class', 'axis-label-main')
+                    .attr('x', 0)
+                    .text(split[0]);
+                text.append('tspan')
+                    .attr('class', 'axis-label-secondary')
+                    .attr('dy', '1em')
+                    .attr('x', 0)
+                    .text(split[1]);
+            });
+        });
+
         selection.call(xAxis);
     }
 
