f9e040a3f270dff1b56b86caa4c630a1c11c70c4
jdhodges431 <jdhodges431@gmail.com>
2016-04-04 10:39:35 +0100

    Nav data sampled to only show 200 points

--- src/assets/js/chart/nav.js
+++ src/assets/js/chart/nav.js
@@ -17,85 +17,88 @@ export default function() {
     var handleCircleCenter = borderWidth + barHeight / 2;
     var handleBarWidth = 2;
     var yExtentPadding = [0, 0.04];
+    var numberOfSamples = 200;
 
     var dispatch = d3.dispatch(event.viewChange);
     var xScale = fc.scale.dateTime();
-    var maskXScale = fc.scale.dateTime();
+    var yScale = d3.scale.linear();
 
-    var navChart = fc.chart.cartesian(xScale, d3.scale.linear())
-      .yTicks(0)
-      .margin({
-          bottom: bottomMargin      // Variable also in navigator.less - should be used once ported to flex
-      });
+    var navChart = fc.chart.cartesian(xScale, yScale)
+        .yTicks(0)
+        .margin({
+            bottom: bottomMargin      // Variable also in navigator.less - should be used once ported to flex
+        });
 
     var viewScale = fc.scale.dateTime();
 
     var area = fc.series.area()
-      .xValue(function(d) { return d.date; })
-      .yValue(function(d) { return d.close; });
+        .xValue(function(d) { return d.date; })
+        .yValue(function(d) { return d.close; });
     var line = fc.series.line()
-      .xValue(function(d) { return d.date; })
-      .yValue(function(d) { return d.close; });
+        .xValue(function(d) { return d.date; })
+        .yValue(function(d) { return d.close; });
     var brush = d3.svg.brush();
     var navMulti = fc.series.multi()
-      .series([area, line, brush])
-      .decorate(function(selection) {
-          var enter = selection.enter();
+        .series([area, line, brush])
+        .decorate(function(selection) {
+            var enter = selection.enter();
 
-          selection.select('.extent')
-            .attr('height', extentHeight)
-            .attr('y', backgroundStrokeWidth / 2);
+            selection.select('.extent')
+                .attr('height', extentHeight)
+                .attr('y', backgroundStrokeWidth / 2);
 
-          // overload d3 styling for the brush handles
-          // as Firefox does not react properly to setting these through less file.
-          enter.selectAll('.resize.w>rect, .resize.e>rect')
-            .attr('width', handleBarWidth)
-            .attr('x', -handleBarWidth / 2);
-          selection.selectAll('.resize.w>rect, .resize.e>rect')
-            .attr('height', barHeight)
-            .attr('y', borderWidth);
-          enter.select('.extent')
-            .attr('mask', 'url("#brush-mask")')
-            .attr('fill', 'url("#brush-gradient")');
+            // overload d3 styling for the brush handles
+            // as Firefox does not react properly to setting these through less file.
+            enter.selectAll('.resize.w>rect, .resize.e>rect')
+                .attr('width', handleBarWidth)
+                .attr('x', -handleBarWidth / 2);
+            selection.selectAll('.resize.w>rect, .resize.e>rect')
+                .attr('height', barHeight)
+                .attr('y', borderWidth);
+            enter.select('.extent')
+                .attr('mask', 'url("#brush-mask")')
+                .attr('fill', 'url("#brush-gradient")');
 
-          // Adds the handles to the brush sides
-          var handles = enter.selectAll('.e, .w');
-          handles.append('circle')
-            .attr('cy', handleCircleCenter)
-            .attr('r', 7)
-            .attr('class', 'outer-handle');
-          handles.append('circle')
-            .attr('cy', handleCircleCenter)
-            .attr('r', 4)
-            .attr('class', 'inner-handle');
-      })
-      .mapping(function(series) {
-          if (series === brush) {
-              brush.extent([
-                  [viewScale.domain()[0], navChart.yDomain()[0]],
-                  [viewScale.domain()[1], navChart.yDomain()[1]]
-              ]);
-              return null;
-          } else {
-              // This stops the brush data being overwritten by the point data
-              return this.data;
-          }
-      });
-
-    var maskYScale = d3.scale.linear();
+            // Adds the handles to the brush sides
+            var handles = enter.selectAll('.e, .w');
+            handles.append('circle')
+                .attr('cy', handleCircleCenter)
+                .attr('r', 7)
+                .attr('class', 'outer-handle');
+            handles.append('circle')
+                .attr('cy', handleCircleCenter)
+                .attr('r', 4)
+                .attr('class', 'inner-handle');
+        })
+        .mapping(function(series) {
+            if (series === brush) {
+                brush.extent([
+                    [viewScale.domain()[0], navChart.yDomain()[0]],
+                    [viewScale.domain()[1], navChart.yDomain()[1]]
+                ]);
+                return null;
+            } else {
+                // This stops the brush data being overwritten by the point data
+                return this.data;
+            }
+        });
 
     var brushMask = fc.series.area()
-      .xValue(function(d) { return d.date; })
-      .yValue(function(d) { return d.close; })
-      .xScale(maskXScale)
-      .yScale(maskYScale);
+        .xValue(function(d) { return d.date; })
+        .yValue(function(d) { return d.close; })
+        .xScale(xScale)
+        .yScale(yScale);
 
     var layoutWidth;
 
+    var sampler = fc.data.sampler.largestTriangleThreeBucket()
+        .x(function(d) { return xScale(d.date); })
+        .y(function(d) { return yScale(d.close); });
+
     function setHide(selection, brushHide) {
         selection.select('.plot-area')
-          .selectAll('.e, .w')
-          .classed('hidden', brushHide);
+            .selectAll('.e, .w')
+            .classed('hidden', brushHide);
     }
 
     function xEmpty(navBrush) {
@@ -104,9 +107,9 @@ export default function() {
 
     function createDefs(selection, data) {
         var defsEnter = selection.selectAll('defs')
-          .data([0])
-          .enter()
-          .append('defs');
+            .data([0])
+            .enter()
+            .append('defs');
 
         defsEnter.html('<linearGradient id="brush-gradient" x1="0" x2="0" y1="0" y2="1"> \
               <stop offset="0%" class="brush-gradient-top" /> \
@@ -121,8 +124,8 @@ export default function() {
             height: navChartHeight
         });
 
-        maskXScale.domain(fc.util.extent().fields('date')(data));
-        maskYScale.domain(fc.util.extent().fields(['low', 'high']).pad(yExtentPadding)(data));
+        xScale.domain(fc.util.extent().fields('date')(data));
+        yScale.domain(fc.util.extent().fields(['low', 'high']).pad(yExtentPadding)(data));
 
         selection.select('mask')
             .datum(data)
@@ -132,24 +135,24 @@ export default function() {
     function nav(selection) {
         var model = selection.datum();
 
+        sampler.bucketSize(Math.max(model.data.length / numberOfSamples, 1));
+        var sampledData = sampler(model.data);
+
         xScale.discontinuityProvider(model.discontinuityProvider);
-        maskXScale.discontinuityProvider(model.discontinuityProvider);
         viewScale.discontinuityProvider(model.discontinuityProvider);
 
-        createDefs(selection, model.data);
+        createDefs(selection, sampledData);
 
         viewScale.domain(model.viewDomain);
 
         var filteredData = util.domain.filterDataInDateRange(
-          fc.util.extent().fields('date')(model.data),
-          model.data);
+            fc.util.extent().fields('date')(sampledData),
+            sampledData);
         var yExtent = fc.util.extent()
-          .fields(['low', 'high']).pad(yExtentPadding)(filteredData);
+            .fields(['low', 'high']).pad(yExtentPadding)(filteredData);
 
-        var brushHide = false;
-
-        navChart.xDomain(fc.util.extent().fields('date')(model.data))
-          .yDomain(yExtent);
+        navChart.xDomain(fc.util.extent().fields('date')(sampledData))
+            .yDomain(yExtent);
 
         brush.on('brush', function() {
             var brushExtentIsEmpty = xEmpty(brush);
@@ -173,19 +176,23 @@ export default function() {
             });
 
         navChart.plotArea(navMulti);
-        selection.call(navChart);
+        selection.datum({data: sampledData}).call(navChart);
 
         // Allow to zoom using mouse, but disable panning
         var zoom = zoomBehavior(layoutWidth)
-          .scale(viewScale)
-          .trackingLatest(model.trackingLatest)
-          .allowPan(false)
-          .on('zoom', function(domain) {
-              dispatch[event.viewChange](domain);
-          });
+            .scale(viewScale)
+            .trackingLatest(model.trackingLatest)
+            .allowPan(false)
+            .on('zoom', function(domain) {
+                dispatch[event.viewChange](domain);
+            });
 
         selection.select('.plot-area')
-          .call(zoom);
+            .datum({
+                data: model.data,
+                discontinuityProvider: model.discontinuityProvider
+            })
+            .call(zoom);
     }
 
     d3.rebind(nav, dispatch, 'on');
@@ -193,8 +200,8 @@ export default function() {
     nav.dimensionChanged = function(container) {
         layoutWidth = util.width(container.node());
         viewScale.range([0, layoutWidth]);
-        maskXScale.range([0, layoutWidth]);
-        maskYScale.range([navChartHeight, 0]);
+        xScale.range([0, layoutWidth]);
+        yScale.range([navChartHeight, 0]);
     };
 
     return nav;
