508384e6e0d4d2eb4b9c31d92e62445fd800029e
James Leftley <jleftley@scottlogic.com>
2016-04-19 12:54:52 +0100

    Update reset button design and hide when tracking latest

--- src/assets/js/menu/navigationReset.js
+++ src/assets/js/menu/navigationReset.js
@@ -8,26 +8,26 @@ export default function() {
     function navReset(selection) {
         var model = selection.datum();
 
-        var resetButton = selection.selectAll('g')
+        var resetButtonGroup = selection.selectAll('g')
             .data([model]);
 
-        var g = resetButton.enter()
+        var resetButtonGroupEnter = resetButtonGroup.enter()
             .append('g')
             .attr('class', 'reset-button')
-            .on('click', function() { dispatch[event.resetToLatest](); });
+            .on('click', dispatch[event.resetToLatest]);
 
-        g.append('path')
+        resetButtonGroupEnter.append('path')
             .attr('d', 'M1.5 1.5h13.438L23 20.218 14.937 38H1.5l9.406-17.782L1.5 1.5z');
 
-        g.append('rect')
+        resetButtonGroupEnter.append('rect')
             .attr({
-                width: 4,
+                width: 5,
                 height: 28,
-                stroke: '#FFF',
                 x: 26,
                 y: 6
             });
-        resetButton.classed('hidden', model.trackingLatest);
+
+        resetButtonGroup.classed('hidden', model.trackingLatest);
     }
 
     d3.rebind(navReset, dispatch, 'on');
