6dee7f01e2f80f2bf06085c7d40d3f76a1122785
James Leftley <jleftley@scottlogic.com>
2016-05-06 14:49:41 +0100

    WIP

--- scripts/get-latest-release.sh
+++ scripts/get-latest-release.sh
@@ -4,14 +4,20 @@ set -e
 rm -rf github-release-downloads
 mkdir github-release-downloads
 
-echo "Downloading latest release..."
-curl -L --silent -H "User-Agent: ScottLogic/BitFlux" https://api.github.com/repos/ScottLogic/BitFlux/releases/latest \
-    | jq -c -r '.assets | .[] | select(.name | contains("dist.tar.gz")) | .url' | sed $'s/\r//' \
-    | xargs -I URL curl --silent -L -H "User-Agent: ScottLogic/BitFlux" -H "Accept: application/octet-stream" -H "Media-Type: application/octet-stream" -o github-release-downloads/dist.tar.gz URL
+echo "Downloading latest release download URL..."
+asset_url=$(curl -L --progress-bar -H "User-Agent: ScottLogic/BitFlux" https://api.github.com/repos/ScottLogic/BitFlux/releases/latest \
+    | jq -c -r '.assets | .[] | select(.name | contains("dist.tar.gz")) | .url')
+echo "Done."
+
+echo "Downloading latest release (${asset_url})..."
+curl --progress-bar -L \
+      -H "User-Agent: ScottLogic/BitFlux" \
+      -H "Accept: application/octet-stream" \
+      -H "Media-Type: application/octet-stream" \
+      -o github-release-downloads/dist.tar.gz ${asset_url}
 
 mkdir github-release-downloads/dist
 
 echo "Extracting latest release..."
 tar -xzf github-release-downloads/dist.tar.gz -C github-release-downloads/dist
-
 echo "Latest release downloaded and extracted."
