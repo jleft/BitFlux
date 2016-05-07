1e6a71a72ed8fbdf119c5778d54279ad57f69a7a
James Leftley <jleftley@scottlogic.com>
2016-05-07 17:20:18 +0100

    wip

--- scripts/get-latest-release.sh
+++ scripts/get-latest-release.sh
@@ -4,17 +4,18 @@ set -e
 rm -rf github-release-downloads
 mkdir github-release-downloads
 
-echo "Downloading latest release download URL..."
-asset_url=$(curl -L --progress-bar -H "User-Agent: ScottLogic/BitFlux" https://api.github.com/repos/ScottLogic/BitFlux/releases/latest \
+echo "Getting latest release download URL..."
+asset_url=$(curl -L --silent -H "User-Agent: ScottLogic/BitFlux" https://api.github.com/repos/ScottLogic/BitFlux/releases/latest \
     | jq -c -r '.assets | .[] | select(.name | contains("dist.tar.gz")) | .url')
-echo "Done."
+echo "Done. (${asset_url})"
 
-echo "Downloading latest release (${asset_url})..."
+echo "Downloading latest release..."
 curl --progress-bar -L \
       -H "User-Agent: ScottLogic/BitFlux" \
       -H "Accept: application/octet-stream" \
       -H "Media-Type: application/octet-stream" \
       -o github-release-downloads/dist.tar.gz ${asset_url}
+echo "Done."
 
 mkdir github-release-downloads/dist
 
