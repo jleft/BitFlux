bfc8270affe5dea4cef167673c369a108639f8a4
James Leftley <jleftley@scottlogic.com>
2016-05-07 18:00:13 +0100

    wip

--- scripts/deploy-site.sh
+++ scripts/deploy-site.sh
@@ -80,7 +80,7 @@ prepare_and_build_develop()
 
 echo "Deploying..."
 
-check_if_should_deploy
+# check_if_should_deploy
 
 echo "Creating temp directory for build..."
 cd site
@@ -107,22 +107,22 @@ printf '{"timestamp":"%s","travis_build_number":"%s","master_version":"%s","deve
 
 echo "Deploying to gh-pages..."
 
-# create a *new* Git repo
-git init
-
-# inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "jleftley@scottlogic.com"
-
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
-git add .
-git commit -m "Deploy to GitHub Pages"
-
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+# # create a *new* Git repo
+# git init
+#
+# # inside this git repo we'll pretend to be a new user
+# git config user.name "Travis CI"
+# git config user.email "jleftley@scottlogic.com"
+#
+# # The first and only commit to this new Git repo contains all the
+# # files present with the commit message "Deploy to GitHub Pages".
+# git add .
+# git commit -m "Deploy to GitHub Pages"
+#
+# # Force push from the current repo's master branch to the remote
+# # repo's gh-pages branch. (All previous history on the gh-pages branch
+# # will be lost, since we are overwriting it.) We redirect any output to
+# # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+# git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
 echo "Done."
